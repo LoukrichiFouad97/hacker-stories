9685f0bdfed979990b3ab448601417ccf89c4a34
fouad97 <loukrichi21@gmail.com>
2020-08-03 16:39:38 +0100

    added style to stories

--- src/Item.jsx
+++ src/Item.jsx
@@ -6,14 +6,16 @@ const Item = ({ title, url, author, num_comments, points, onRemoveItem }) => {
 	}
 
 	return (
-		<div>
+		<div className=" container bg-warning my-1 p-3 d-flex justify-content-between align-items-center rounded">
 			<span>
 				<a href={url}>{title}</a>
 			</span>
 			<span>{author}</span>
 			<span>{num_comments}</span>
 			<span>{points}</span>
-			<button onClick={handleRemoveStory}>Dismiss</button>
+			<button className="btn btn-danger" onClick={handleRemoveStory}>
+				Dismiss
+			</button>
 		</div>
 	);
 };
